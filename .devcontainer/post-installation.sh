495038551f8de04f9ff6d8d407c3f9a481a641e3
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-06-04 11:40:46 +0000

    change cli.completion.prompted to true

--- .devcontainer/post-installation.sh
+++ .devcontainer/post-installation.sh
@@ -2,4 +2,4 @@
 
 echo "Enable Angular CLI autocompletion"
 ng completion script | sudo tee /usr/share/bash-completion/completions/ng > /dev/null
-ng config -g cli.completion.prompted false
+ng config -g cli.completion.prompted true
