df632afa8b58077d8b5c215e699b8437852fa807
Andrej R. <10363490+jodabyte@users.noreply.github.com>
2026-06-14 12:47:33 +0000

    add a dialog to create a shopping list

--- src/app/features/catalog/catalog.ts
+++ src/app/features/catalog/catalog.ts
@@ -1,24 +1,20 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { OptionsMenu } from './options-menu/options-menu';
+import { MatFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatActionList } from '@angular/material/list';
+import { OptionsMenu } from '@features/catalog/options-menu/options-menu';
 
 @Component({
   selector: 'app-catalog',
-  imports: [MatListModule, MatIconModule, MatButtonModule],
+  imports: [MatActionList, MatFabButton, MatIcon],
   templateUrl: './catalog.html',
   styleUrl: './catalog.scss',
 })
 export class Catalog {
-  #optionsMenu: MatBottomSheet;
-
-  constructor(optionsMenu: MatBottomSheet) {
-    this.#optionsMenu = optionsMenu;
-  }
+  constructor(private optionsMenu: MatBottomSheet) {}
 
   openOptionsMenu(): void {
-    this.#optionsMenu.open(OptionsMenu);
+    this.optionsMenu.open(OptionsMenu);
   }
 }
--- src/app/features/catalog/options-menu/options-menu.ts
+++ src/app/features/catalog/options-menu/options-menu.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatDialog } from '@angular/material/dialog';
+import { MatActionList } from '@angular/material/list';
+import { Wizard } from '@features/collection/shopping-list/wizard/wizard';
 
 @Component({
   selector: 'app-options-menu',
-  imports: [],
+  imports: [MatActionList],
   templateUrl: './options-menu.html',
   styleUrl: './options-menu.scss',
 })
-export class OptionsMenu {}
+export class OptionsMenu {
+  constructor(
+    private bottomSheetRef: MatBottomSheetRef<OptionsMenu>,
+    private wizardDialog: MatDialog,
+  ) {}
+
+  createShoppingList(): void {
+    this.wizardDialog.open(Wizard);
+    this.bottomSheetRef.dismiss();
+  }
+}
